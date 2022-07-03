76982c6ea9fc769fbfcefacf25449fa3c3e715ed
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-07-03 12:31:12 -0700

    created Constants.ts and change PORT to 3333.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from './Constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
