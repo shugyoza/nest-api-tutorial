ab20712e8ae9d664b23038325cdb3492226cfa64
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-07-03 14:44:21 -0700

    import AuthService to Controller for Dependency Injection, and defined empty methods: login and signup.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller({})
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService: AuthService) {}
+}
